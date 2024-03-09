d7845b216113df1505a3ec63e09b96aeb0a1d2fb
Andre Pretorius <pretorius.dre@gmail.com>
2024-03-09 21:30:59 +0800

    Release to 1.46

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='populate-synapse-nb',
-    version='1.45',
+    version='1.46',
     packages=find_packages(),
     install_requires=[
         'pathlib',
