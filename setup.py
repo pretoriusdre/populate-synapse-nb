a677a34a82f45b0aceb3c462bb4c858d2b5583e6
Andre Pretorius <pretorius.dre@gmail.com>
2024-03-09 17:46:38 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='populate-synapse-nb',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pathlib',
+        'json',
+        'copy',
+    ],
+    author='Andre Pretorius',
+    author_email='pretorius.dre+github@gmail.com',
+    description='A package to populate Azure Synapse Analytics Notebooks (json format) with code from Python source files.',
+    url='https://github.com/pretoriusdre/populate-synapse-nb'
