4c3775eabdb5cc63a57431f712f4f071172471af
Andre Pretorius <pretorius.dre@gmail.com>
2024-03-09 20:54:58 +0800

    Update 1.42, minor fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='populate-synapse-nb',
-    version='1.41',
+    version='1.42',
     packages=find_packages(),
     install_requires=[
         'pathlib',
