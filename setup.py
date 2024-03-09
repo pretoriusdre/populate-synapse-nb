5d781a8e7fb930601c43cb5c7d0cd3e68b76f07d
Andre Pretorius <pretorius.dre@gmail.com>
2024-03-09 18:36:50 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='populate-synapse-nb',
-    version='1.01',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'pathlib',
